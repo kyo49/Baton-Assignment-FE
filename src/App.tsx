687d82c7b802fe9d99cc2bfa6f1e87da78b7989c
kyo49 <kyo8747@naver.com>
2023-09-06 01:23:27 +0900

    [FEAT] 만든 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Global } from '@emotion/react';
 import { reset } from './styles/globalStyle';
+import Background from './components/Background';
+import Content from './components/Content';
 
 function App() {
   return (
     <>
       <Global styles={reset} />
+      <Background />
+      <Content />
     </>
   );
 }
