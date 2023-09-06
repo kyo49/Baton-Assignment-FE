6afc3dacb5c16914f033129ef5923dc4751ee117
kyo49 <kyo8747@naver.com>
2023-09-07 08:50:40 +0900

    [FEAT] Balloons 연결

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,19 +1,31 @@
-import React from 'react';
-import { CastleIcon, HeartBallonIcons } from '../../assets';
+import React, { useState } from 'react';
+
 import * as S from './styles';
+import { CastleIcon, HeartBallonIcons } from '../../assets';
+import Balloon from '../Balloons';
 
 const Content = () => {
+  const [balloonCount, setBalloonCount] = useState(10);
+
+  const onAddBalloon = () => {
+    setBalloonCount(balloonCount + 1);
+  };
   return (
-    <S.ContentArea>
-      <S.CastleImg src={CastleIcon} />
-      <S.CircleBalloon>
-        <span>기존에 있던 풍선은 없앨 수 없어요.</span>
-      </S.CircleBalloon>
-      <S.HeartBalloon>
-        <img src={HeartBallonIcons} />
-        <span>성을 클릭해 풍선을 추가해 보세요.</span>
-      </S.HeartBalloon>
-    </S.ContentArea>
+    <>
+      <S.ContentArea>
+        <S.CastleImg src={CastleIcon} onClick={onAddBalloon} />
+        <S.CircleBalloon>
+          <span>기존에 있던 풍선은 없앨 수 없어요.</span>
+        </S.CircleBalloon>
+        <S.HeartBalloon>
+          <img src={HeartBallonIcons} />
+          <span>성을 클릭해 풍선을 추가해 보세요.</span>
+        </S.HeartBalloon>
+      </S.ContentArea>
+      {Array.from({ length: balloonCount }).map((_, index) => (
+        <Balloon key={index} />
+      ))}
+    </>
   );
 };
 
