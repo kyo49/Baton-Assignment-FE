c2c3948c692182250c25b96d3aa72a48d5fa0274
kyo49 <kyo8747@naver.com>
2023-09-07 18:21:17 +0900

    [FIX] 풍선 문구 수정

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -15,11 +15,11 @@ const Content = () => {
       <S.ContentArea>
         <S.CastleImg src={CastleIcon} onClick={onAddBalloon} />
         <S.CircleBalloon>
-          <span>기존에 있던 풍선은 없앨 수 없어요.</span>
+          <span>Double-click to delete balloons</span>
         </S.CircleBalloon>
         <S.HeartBalloon>
           <img src={HeartBallonIcons} />
-          <span>성을 클릭해 풍선을 추가해 보세요.</span>
+          <span>Click Castle to add a balloon</span>
         </S.HeartBalloon>
       </S.ContentArea>
       {Array.from({ length: balloonCount }).map((_, index) => (
